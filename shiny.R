2c9e9a0c3dc3f46c94395f8bd76c7d91d285a3cd
blaneart <blanar.a.v@gmail.com>
2020-04-13 01:23:49 +0200

    Histogram added

--- shiny.R
+++ shiny.R
@@ -5,8 +5,9 @@ ui <- fluidPage(tabsetPanel(
   tabPanel(title = "GC content",
   plotOutput("GC")), 
   tabPanel(title = "Amino Acid Table",
-           DT::dataTableOutput("AA")))
-)
+           DT::dataTableOutput("AA")), 
+  tabPanel(title = "Protein Weights",
+           plotOutput("PW"))))
 
 server <- function(input, output){
   output$GC <- renderPlot({
@@ -52,6 +53,37 @@ server <- function(input, output){
     df <- subset(df, !(df$length < 20))
     rownames(df) <- 1:nrow(df)
     df
+  })
+  output$PW <- renderPlot({
+    library(seqinr)
+    library(Biostrings)
+    library(protr)
+    
+    dengue <- read.fasta("./cov.fna")
+    dengueseq <- dengue[[1]]
+    amin_acid <- translate(dengueseq)
+    str <- NULL
+    df <- read.table(text = "", colClasses = c("character", "integer"),
+                     col.names = c("sequence", "length"))
+    for (i in 1:length(amin_acid))
+    {
+      if ((amin_acid[i] == "*" || i == length(amin_acid)) && !is.null(str) )
+      {
+        df[nrow(df) + 1,] = list(str, nchar(str))
+        str = NULL
+      }
+      else if (amin_acid[i] != "*")
+        str <- paste(str, amin_acid[i], sep="")
+    }
+    df <- subset(df, !(df$length < 20))
+    rownames(df) <- 1:nrow(df)
+    weight <- read.table(text = "", colClasses = c("double"),
+                     col.names = c("weight"))
+    for (i in 1:length(df$sequence))
+      weight[nrow(weight) + 1,] = c(mw(seq = df[i,1], monoisotopic = FALSE))
+    hist(weight$weight, breaks = 80, col = "#75AADB", border = "white",
+         xlab = "The molecular weight, Da",
+         main = "Distribution of molecular weight")
   })
 }
 
